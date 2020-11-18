57fcc8d3aff9ded605739479a3c69f335c3ef2ca
Silviana <silvistudies@gmail.com>
2020-11-19 01:03:08 +0800

    finished 7 part tutorial

--- mnistwebapp/settings.py
+++ mnistwebapp/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7_6$#n)-m22c67p8(*6h$r6o@r@xn5x#tyqxcu1zu+cz$u7ch1'
+with open('/code/mnistwebapp/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
